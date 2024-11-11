f9d939dccec88fd8e6a24b926d0fc515cec40a5f
WillianTaikiAraki <willian.araki@unifesp.br>
2024-11-11 18:20:20 -0300

    Create 1004_Produto_simples.c

---
+++ 1004_Produto_simples.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+int A, B, PROD;
+
+// Lê os dois valores inteiros
+scanf("%d", &A);
+scanf("%d", &B);
+
+// Calcula o produto dos dois valores
+PROD = A * B;
+
+printf("PROD = %d\n", PROD);
+
+    return 0;
+}
